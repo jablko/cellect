fa472ee1eb8bcb805cf5e21bd1a6b0e1532126fa
Jack Bates <jack@nottheoilrig.com>
2013-04-13 14:04:54 -0700

    Use setTimeout() to check if user script installed/enabled after Chrome runs it

--- greasemonkey/test/script.js
+++ greasemonkey/test/script.js
@@ -1,22 +1,26 @@
 jQuery(function ($) {
-  var browserBot = new BrowserBot();
 
-  var $table = $('#fixture table');
+  // After Chrome user script
+  setTimeout(function () {
+    var browserBot = new BrowserBot();
 
-  // Call vendorPropName() before style.userSelect = ...
-  $table.css('user-select');
+    var $table = $('#fixture table');
 
-  browserBot.triggerMouseEvent($table[0].rows[1].cells[1], 'mousedown', true);
-  browserBot.triggerMouseEvent($table[0].rows[1].cells[1], 'mouseout', true);
-  browserBot.triggerMouseEvent($table[0].rows[1].cells[2], 'mouseover', true);
+    // Call vendorPropName() before style.userSelect = ...
+    $table.css('user-select');
 
-  if ($table.css('cursor') === 'cell') {
-    $('<div style="background: #0f0">Checking <strong>installed</strong> script...</div>').prependTo(document.body);
-  } else {
-    $('<div style="background: #f00">Checking <strong>development</strong> script...</div>').prependTo(document.body);
+    browserBot.triggerMouseEvent($table[0].rows[1].cells[1], 'mousedown', true);
+    browserBot.triggerMouseEvent($table[0].rows[1].cells[1], 'mouseout', true);
+    browserBot.triggerMouseEvent($table[0].rows[1].cells[2], 'mouseover', true);
 
-    $('<script src="../cellect.user.js">').appendTo(document.body);
-  }
+    if ($table.css('cursor') === 'cell') {
+      $('<div style="background: #0f0">Checking <strong>installed</strong> script...</div>').prependTo(document.body);
+    } else {
+      $('<div style="background: #f00">Checking <strong>development</strong> script...</div>').prependTo(document.body);
 
-  browserBot.triggerMouseEvent($table[0].rows[1].cells[2], 'mouseup', true);
+      $('<script src="../cellect.user.js">').appendTo(document.body);
+    }
+
+    browserBot.triggerMouseEvent($table[0].rows[1].cells[2], 'mouseup', true);
+  });
 });
