46100b97c9a3464ff07e155e1c8350a08cadbcba
Jack Bates <jack@nottheoilrig.com>
2013-04-13 07:55:09 -0700

    Check if user script is installed/enabled and load it if not

---
+++ greasemonkey/test/script.js
@@ -0,0 +1,22 @@
+jQuery(function ($) {
+  var browserBot = new BrowserBot();
+
+  var $table = $('#fixture table');
+
+  // Call vendorPropName() before style.userSelect = ...
+  $table.css('user-select');
+
+  browserBot.triggerMouseEvent($table[0].rows[1].cells[1], 'mousedown', true);
+  browserBot.triggerMouseEvent($table[0].rows[1].cells[1], 'mouseout', true);
+  browserBot.triggerMouseEvent($table[0].rows[1].cells[2], 'mouseover', true);
+
+  if ($table.css('cursor') === 'cell') {
+    $('<div>Checking <strong>installed</strong> script...</div>').prependTo(document.body);
+  } else {
+    $('<div>Checking <strong>development</strong> script...</div>').prependTo(document.body);
+
+    $('<script src="../cellect.user.js">').appendTo(document.body);
+  }
+
+  browserBot.triggerMouseEvent($table[0].rows[1].cells[2], 'mouseup', true);
+});
